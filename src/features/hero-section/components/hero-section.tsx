4c81d0eff5a82f26d2fe2d97aff29ba45688229b
codesbyree <bahreesaepul1@gmail.com>
2026-06-03 07:00:29 +0700

    feat: hero-section

---
+++ src/features/hero-section/components/hero-section.tsx
@@ -0,0 +1,26 @@
+import Button from "../../../components/ui/button";
+
+import ilustrationWorking from "../../../assets/images/illustration-working.svg";
+
+export default function HeroSection() {
+  return (
+    <section id="hero-section" className="lg:overflow-hidden">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:max-w-4xl xl:max-w-5xl lg:mx-auto lg:py-8">
+        <div className="p-4 flex flex-col items-center row-start-2 lg:row-start-1 lg:p-0 lg:items-start lg:justify-center">
+          <h1 className="font-bold text-gray-900 text-4xl text-center mb-3 lg:text-left lg:text-5xl lg:leading-14 xl:text-6xl xl:leading-17">
+            More than just <br className="lg:block hidden" /> shorter links
+          </h1>
+          <p className="text-gray-500 font-medium text-center mb-6 lg:text-left">
+            Build your brand's recognition and get detailed <br className="lg:block hidden" /> insights on how your links are performing.
+          </p>
+
+          <Button className="w-max">Get Started</Button>
+        </div>
+
+        <div className="row-start-1 w-full overflow-hidden lg:col-start-2 lg:overflow-visible">
+          <img src={ilustrationWorking} alt="" className="w-[130vw] lg:w-170 max-w-none h-auto object-cover translate-x-6" />
+        </div>
+      </div>
+    </section>
+  );
+}
